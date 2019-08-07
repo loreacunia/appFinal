99c1bd9c234a2d018e192ce77e93d08bcfdc68f9
scarlet <scarlet.tale11@gmail.com>
2019-08-07 12:42:26 -0300

    add data

---
+++ public/partidos.js
@@ -0,0 +1,162 @@
+var partidos = [{
+    
+    nombre: "U1 v U4",
+    mes: "06",
+    horario: "9:30 AM",
+    lugar: " Greenbay",
+    dia: "01",
+    url: "https://www.google.com/maps/d/embed?mid=1hZNOyJTQ-rF98ZLixGy8m37H5mv-3fN"
+  },
+  {
+    nombre: "U2 v U3",
+    mes: "06",
+    horario: "1:30 PM",
+    lugar: " Greenbay",
+    dia: "01",
+    url: "https://www.google.com/maps/d/embed?mid=1hZNOyJTQ-rF98ZLixGy8m37H5mv-3fN"
+
+  },
+
+  {
+    nombre: "U5 v U6",
+    mes: "06",
+    horario: "9:30 AM",
+    lugar: " Howard A Yeager",
+    dia: "08",
+    url: "https://www.google.com/maps/d/embed?mid=1wbhmRjZGOF9eRDn6oIIciuXqQd2gi_Yg"
+
+  },
+  {
+    nombre: "U6 v U1",
+    mes: "06",
+    horario: "1:30 PM",
+    lugar: " Marjorie P Hart",
+    dia: "08",
+    url: "https://www.google.com/maps/d/embed?mid=1SID7QdfDWObUWMXQOsC6PyJM3FKPEW9K"
+
+  },
+  {
+    nombre: "U2 v U4",
+    mes: "06",
+    horario: "9:30 AM",
+    lugar: " North",
+    dia: "15",
+    url: "https://www.google.com/maps/d/embed?mid=1KZCKiaP9yNRrGkWK52RmnKaF9gy8M8xB"
+  },
+  {
+    nombre: "U3 v U5",
+    mes: "06",
+    horario: "1:30 PM",
+    lugar: " AJ Katzenmaier",
+    dia: "15",
+    url: "https://www.google.com/maps/d/embed?mid=1QnaR8ef5aC4r4C255jypOHspc2YYhLka"
+
+  },
+  {
+    nombre: "U1 v U3",
+    mes: "06",
+    horario: "9:30 AM",
+    lugar: " South",
+    dia: "22",
+    url: "https://www.google.com/maps/d/embed?mid=1xAgVyYoWCHYLd_WxuoHsMA16JfzNQASg"
+  },
+  {
+    nombre: "U2 v U6",
+    mes: "06",
+    horario: "1:30 PM",
+    lugar: " Howard A Yeager",
+    dia: "22",
+    url: "https://www.google.com/maps/d/embed?mid=1wbhmRjZGOF9eRDn6oIIciuXqQd2gi_Yg"
+  },
+  {
+    nombre: "U4 v U5",
+    mes: "06",
+    horario: "9:30 AM",
+    lugar: " Greenbay",
+    dia: "29",
+    url: "https://www.google.com/maps/d/embed?mid=1hZNOyJTQ-rF98ZLixGy8m37H5mv-3fN"
+
+  },
+
+  {
+    nombre: "U2 v U5",
+    mes: "07",
+    horario: "9:30 AM",
+    lugar: "Marjorie P Hart",
+    dia: "06",
+    url: "https://www.google.com/maps/d/embed?mid=1SID7QdfDWObUWMXQOsC6PyJM3FKPEW9K"
+  },
+  {
+    nombre: "U1 v U6",
+    mes: "07",
+    horario: "1:00 PM",
+    lugar: "South",
+    dia: "06",
+    url: "https://www.google.com/maps/d/embed?mid=1xAgVyYoWCHYLd_WxuoHsMA16JfzNQASg"
+  },
+  {
+    nombre: "U3 v U4",
+    mes: "07",
+    horario: "9:30 AM",
+    lugar: "Howard A Yeager	",
+    dia: "08",
+    url: "https://www.google.com/maps/d/embed?mid=1wbhmRjZGOF9eRDn6oIIciuXqQd2gi_Yg"
+  },
+  {
+    nombre: "U5 v U1",
+    mes: "07",
+    horario: "1:00 PM",
+    lugar: "Greenbay",
+    dia: "08",
+    url: "https://www.google.com/maps/d/embed?mid=1hZNOyJTQ-rF98ZLixGy8m37H5mv-3fN"
+  },
+  {
+    nombre: "U6 v U3",
+    mes: "07",
+    horario: "9:30 AM",
+    lugar: "North",
+    dia: "20",
+    url: "https://www.google.com/maps/d/embed?mid=1KZCKiaP9yNRrGkWK52RmnKaF9gy8M8xB"
+  },
+  {
+    nombre: "U2 v U4",
+    mes: "07",
+    horario: "1:00 PM",
+    lugar: "Marjorie P Hart",
+    dia: "20",
+    url: "https://www.google.com/maps/d/embed?mid=1SID7QdfDWObUWMXQOsC6PyJM3FKPEW9K"
+  },
+  {
+    nombre: "U3 v U1",
+    mes: "07",
+    horario: "9:30 AM",
+    lugar: "AJ Katzenmaier",
+    dia: "27",
+    url: "https://www.google.com/maps/d/embed?mid=1QnaR8ef5aC4r4C255jypOHspc2YYhLka"
+  },
+  {
+    nombre: "U5 v U6",
+    mes: "07",
+    horario: "1:00 PM",
+    lugar: "Howard A Yeager",
+    dia: "27",
+    url: "https://www.google.com/maps/d/embed?mid=1wbhmRjZGOF9eRDn6oIIciuXqQd2gi_Yg"
+  },
+  {
+    nombre: "U6 v U1",
+    mes: "08",
+    horario: "9:30 AM",
+    lugar: " Marjorie P Hart",
+    dia: "07",
+    url: "https://www.google.com/maps/d/embed?mid=1SID7QdfDWObUWMXQOsC6PyJM3FKPEW9K"
+
+  },
+  {
+    nombre: "U5 v U6",
+    mes: "08",
+    horario: "1:00 PM",
+    lugar: "AJ Katzenmaier",
+    dia: "07",
+    url: "https://www.google.com/maps/d/embed?mid=1QnaR8ef5aC4r4C255jypOHspc2YYhLka"
+  },
+];
